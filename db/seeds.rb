5df53ea16ab0757d830dad281725750dd0bedcdc
doulighan <doulighan@gmail.com>
2017-05-23 12:08:48 -0400

    done

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: "Talisman", artist_name: "Air", released: true, release_year: 1966, genre: "Post-Rock")
+Song.create(title: "Bair", artist_name: "get", released: false, genre: "yolo-Rock")
